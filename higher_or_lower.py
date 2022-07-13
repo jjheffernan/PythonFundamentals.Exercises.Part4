30f48c58bb68df4fa36652795d9abc8ac34b4d3d
jjheffernan <jjheffernan99@gmail.com>
2022-07-13 00:37:16 -0400

    completed game functionality

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -19,23 +19,28 @@ print("""
 def computer_guess():
     global comp_guess
     comp_guess = random.randint(1, 10)
+    return comp_guess
 
 
 def number_input():
     global player_num
-    player_num == input("guess a number between 1 and 10: ")
+    player_num = int(input("guess a number between 1 and 10: "))
     # print(player_num)
     return player_num
 
 
 def number_return():
-    print(player_num)
-    print(comp_guess)
-    return player_num
+    pass
+    # print(player_num)
+    # print(comp_guess)
+    # return player_num
 
 
 def compare_guess():
     global game_status
+    global player_num
+    global comp_guess
+    # player_choice = player_num
     if player_num > comp_guess:
         print("Too High")
     elif player_num < comp_guess:
@@ -48,24 +53,43 @@ def compare_guess():
 def play_again():
     global game_status
     choice = input("do you want to keep playing? (Y/N) ")
-    if choice.lower == 'y':
-        game_status = True
-        game()
-    else:
+    if choice == 'n':
         game_status = False
+
+    elif choice == 'y':
+        game_status = True
+
+    else:
+        print("Error!")
 
 
 def game():
     global game_status
+    global player_num
+    global comp_guess
     while game_status:
-        number_input()
+        # print(comp_guess)
+        player_num = number_input()
+
         compare_guess()
         number_return()
-        play_again()
+        if game_status:
+            play_again()
+
+        # choice = input("do you want to keep playing? (Y/N) ")
+        # if choice == 'n':
+        #     game_status = False
+        # elif choice == 'y':
+        #     game_status = True
+        #     game()
+        # else:
+        #     print("Error!")
 
 
 def main():
     """main function"""
+    global comp_guess
+    comp_guess = computer_guess()
     game()
     # number_input()
     # number_return()
