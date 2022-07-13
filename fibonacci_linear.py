e51f1b6950133e60a9e761efad3c57c6f0397bf8
jjheffernan <jjheffernan99@gmail.com>
2022-07-13 00:46:04 -0400

    completes output, used dict as well, could change.

---
+++ fibonacci_linear.py
@@ -0,0 +1,38 @@
+"""
+ported over from original build of fibonacci recursive, that wasnt so recursive
+made by jjh on 13 Jul 2022
+"""
+
+fib_Array = {0: 0, 1: 1}  # declare dictionary
+# this done to avoid going into negative ints
+
+
+def fibonacci(n):
+    # n = 0
+    x = 1
+    y = 1
+    z = 0
+
+    if n in fib_Array:
+        # print(f"{n} is not a correct input ")
+        return fib_Array[n]
+    else:
+        for i in range(1, n):
+            z = x + y
+            x = y
+            y = z
+            fib_Array[i] = z
+
+    # elif n == 1 or n == 2:
+        # return 0
+        # print(n)
+    # else:
+    # fib_Array[n] = fibonacci(n-1) + fibonacci(n-2)
+
+    return fib_Array
+
+
+# error case escape
+fibonacci(int(input('How far into fibonacci do you want to go? ')))
+print(fib_Array)
+
